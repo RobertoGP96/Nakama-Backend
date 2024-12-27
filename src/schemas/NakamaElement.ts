3a8966dc1a72a1f7f94bd2241a9c1a9d10767e1f
RobertoGP96 <trolbertogp96@gmail.com>
2024-12-27 14:02:26 -0500

    fixing validation element

--- src/schemas/NakamaElement.ts
+++ src/schemas/NakamaElement.ts
@@ -49,7 +49,7 @@ export const ElementSchema = z.object({
     required_error: "Abstract is required",
   }),
 
-  genres: GenreSchema.required(),
+  genres: GenreSchema,
   credits: CreditsSchema,
   externalids: ExternalIdsSchema,
   metadata: MetadataSchema,
